448458b9863562d8ab9dd4204c15e3f0d96286a6
Ștefan Țîncu <stefantnc11@gmail.com>
2022-12-10 22:50:50 +0200

    Start adding API endpoints for album resources
    - Get all albums
    - Get album by id
    
    Resolves: CU-38nfhc0

--- src/main/java/com/tinnitussounds/cms/album/AlbumController.java
+++ src/main/java/com/tinnitussounds/cms/album/AlbumController.java
@@ -1,23 +1,35 @@
 package com.tinnitussounds.cms.album;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class AlbumController {
     @Autowired
-    private AlbumRepository albumRepository;
+    AlbumRepository albumRepository;
 
     @GetMapping("/api/admin/albums")
     public ResponseEntity<List<Album>> getAlbums() {
         return ResponseEntity.status(200).body(albumRepository.findAll());
+    }
+
+    @GetMapping("/api/admin/albums/album{id}")
+    public ResponseEntity<Album> getAlbum(@RequestParam("id") String id) {
+        Optional<Album> album = albumRepository.findById(id);
+        if(album.isEmpty()) {
+            return  ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok().body(album.get());
+        }
     }
 
     @PostMapping("/api/admin/albums")

46c89f67fc762a62be770660607bcd84147158d3
Ștefan Țîncu <stefantnc11@gmail.com>
2022-12-10 22:49:06 +0200

    Add authentication APIs
    - Admin login
    - Generate pre-authenticated request to access object storage (mobile app users)
    
    Resolves: CU-38nfhc0

--- src/main/java/com/tinnitussounds/cms/auth/AuthController.java
+++ src/main/java/com/tinnitussounds/cms/auth/AuthController.java
@@ -1,28 +1,93 @@
 package com.tinnitussounds.cms.auth;
 
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oracle.bmc.objectstorage.model.PreauthenticatedRequest;
+import com.tinnitussounds.cms.services.ObjectStorageService;
 import com.tinnitussounds.cms.services.TokenService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    AdminRepository adminAuthRepository;
+
     private final TokenService tokenService;
+    private final ObjectStorageService objectStorageService;
 
-    public AuthController(TokenService tokenService) {
+    public AuthController(TokenService tokenService, ObjectStorageService objectStorageService) {
         this.tokenService = tokenService;
+        this.objectStorageService = objectStorageService;
     }
 
-    @PostMapping("/api/login")
-    public ResponseEntity<?> authenticateAdmin(Authentication auth) {
-        return ResponseEntity.status(200).body(tokenService.generateToken(auth));
+    @PostMapping("/login")
+    public ResponseEntity<HashMap<String, String>> authenticateAdmin(Authentication auth) throws JSONException {
+        HashMap<String, String> map = new HashMap<String, String>();
+        String token = tokenService.generateToken(auth);
+        String preauthReq = "";
+        List<Admin> admins = adminAuthRepository.findAll();
+        for(Admin admin : admins) {
+            if(auth.getName().equals(admin.getUser())) {
+                preauthReq = admin.getStoragePreauth(); 
+                break;
+            }
+        }
+        map.put("token", token);
+        map.put("preauthreq", preauthReq);
+        return ResponseEntity.status(200).body(map);
     }
 
-    @GetMapping("/api/admin/test")
-    public String home() {
-        return "Bravo ai ajuns aici. Acum poti sa o sugi.";
+    @PostMapping("/api/objectstorage/preauth")
+    public ResponseEntity<?> createObjectStoragePreauthReq(@RequestBody String credentials)
+            throws BadCredentialsException {
+
+        //Extract JSON to map
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> map = new HashMap<String, String>();
+        try {
+            map = mapper.readValue(credentials, new TypeReference<Map<String, String>>() {
+            });
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        String userId = map.get("userId");
+        Optional<User> temp = userRepository.findById(userId);
+
+        //Check if user exists
+        if (temp.isPresent()) {
+            User user = temp.get();
+            Optional<PreauthenticatedRequest> preauthReq = objectStorageService.createPreauthRequest("resources",
+                    userId);
+            if (preauthReq.isPresent()) {
+                //Insert pre-authenticated request field
+                user.setStoragePreauth(preauthReq.get());
+                userRepository.save(user);
+                return ResponseEntity.ok().body("Pre-authenticated request created");
+            } else {
+                return ResponseEntity.internalServerError()
+                        .body("Could not create pre-authenticated request for user");
+            }
+        } else {
+            return ResponseEntity.status(401).body("User not found");
+        }
     }
 }
--- src/main/java/com/tinnitussounds/cms/auth/ExternalBasicAuthenticationProvider.java
+++ src/main/java/com/tinnitussounds/cms/auth/ExternalBasicAuthenticationProvider.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Component
 public class ExternalBasicAuthenticationProvider implements AuthenticationProvider {
     @Autowired
-    AuthRepository authRepository;
+    AdminRepository authRepository;
 
     @Override
     public Authentication authenticate(Authentication auth)

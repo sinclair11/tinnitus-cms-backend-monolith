d8a5acda191be3e6afd5bbc6c8f7d94e2fd93195
Ștefan Țîncu <stefantnc11@gmail.com>
2022-12-14 15:59:55 +0200

    Add entity class and service for token refresh
    Resolves: CU-39xf6kb

--- src/main/java/com/tinnitussounds/cms/config/Constants.java
+++ src/main/java/com/tinnitussounds/cms/config/Constants.java
@@ -2,4 +2,5 @@ package com.tinnitussounds.cms.config;
 
 public class Constants {
     public final static String objectStorageNamespace = "lrvrqsienkzb";
+    public final static long tokenDuration = 1000 * 3600;
 }

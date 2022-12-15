3755a6ec39900517163e69ed89905df605246ec7
Ștefan Țîncu <stefantnc11@gmail.com>
2022-12-15 10:17:11 +0200

    Minor changes in properties and constants

--- src/main/java/com/tinnitussounds/cms/config/Constants.java
+++ src/main/java/com/tinnitussounds/cms/config/Constants.java
@@ -3,4 +3,5 @@ package com.tinnitussounds.cms.config;
 public class Constants {
     public final static String objectStorageNamespace = "lrvrqsienkzb";
     public final static long tokenDuration = 1000 * 3600;
+    public final static String albumPath = System.getProperty("user.dir") + "/albums";
 }

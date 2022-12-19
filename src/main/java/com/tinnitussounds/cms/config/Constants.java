b3df949772cb46175bf9d56976026ad0d6239849
Ștefan Țîncu <stefantnc11@gmail.com>
2022-12-19 11:18:28 +0200

    Add root paths for albums and categories resources

--- src/main/java/com/tinnitussounds/cms/config/Constants.java
+++ src/main/java/com/tinnitussounds/cms/config/Constants.java
@@ -3,5 +3,6 @@ package com.tinnitussounds.cms.config;
 public class Constants {
     public final static String objectStorageNamespace = "lrvrqsienkzb";
     public final static long tokenDuration = 1000 * 3600;
-    public final static String albumPath = System.getProperty("user.dir") + "/albums";
+    public final static String albumsPath = System.getProperty("user.dir") + "/assets/albums";
+    public final static String categoriesPath = System.getProperty("user.dir") + "/assets/categories";
 }

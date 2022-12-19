895dfc1d24d83543037066f41e9730d871d0be85
Ștefan Țîncu <stefantnc11@gmail.com>
2022-12-19 11:19:24 +0200

    Finalize implementation for album APIs
    Resolves: CU-3a37bdz

--- src/main/java/com/tinnitussounds/cms/album/AlbumController.java
+++ src/main/java/com/tinnitussounds/cms/album/AlbumController.java
@@ -7,26 +7,30 @@ import java.util.Optional;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.tinnitussounds.cms.services.ObjectStorageService;
+import com.tinnitussounds.cms.config.Constants;
+import com.tinnitussounds.cms.services.StorageService;
 
 @Controller
 public class AlbumController {
     @Autowired
     AlbumRepository albumRepository;
+    private StorageService storageService;
 
-    private ObjectStorageService objectStorageService;
-
-
-    public AlbumController(AlbumRepository albumRepository, ObjectStorageService objectStorageService) {
-        this.objectStorageService = objectStorageService;
+    public AlbumController(StorageService storageService) {
+        this.storageService = storageService;
     }
 
     @GetMapping("/api/admin/albums")
@@ -34,8 +38,8 @@ public class AlbumController {
         return ResponseEntity.status(200).body(albumRepository.findAll());
     }
 
-    @GetMapping("/api/admin/albums/album{id}")
-    public ResponseEntity<Album> getAlbum(@RequestParam("id") String id) {
+    @GetMapping("/api/admin/album/{id}")
+    public ResponseEntity<Album> getAlbum(@PathVariable("id") String id) {
         Optional<Album> album = albumRepository.findById(id);
         if (album.isEmpty()) {
             return ResponseEntity.notFound().build();
@@ -44,27 +48,98 @@ public class AlbumController {
         }
     }
 
-    @PostMapping("/api/admin/albums")
+    @PostMapping("/api/admin/album")
     public ResponseEntity<String> registerAlbum(@RequestBody Album album) {
         album.setUploadDate(DateTime.now().toDateTime(DateTimeZone.UTC).toString());
-        albumRepository.insert(album);
+        Album dbAlbum = albumRepository.insert(album);
 
-        return ResponseEntity.ok().body("Album successfully registered in database");
+        return ResponseEntity.ok().body(dbAlbum.getId());
     }
 
-    @PostMapping("/api/admin/albums/files")
-    public ResponseEntity<String> uploadFile(@RequestParam("type") String fileType, 
-    @RequestParam("name") String fileName,
-    @RequestParam("file") MultipartFile file) {
+    @PutMapping("/api/admin/album")
+    public ResponseEntity<String> updateAlbum(@RequestBody AlbumEdit albumEdit) {
+        Optional<Album> albumDb = albumRepository.findById(albumEdit.getId());
+
+        if(albumDb.isPresent()) {
+            Album album = albumDb.get();
+            album.setDescription(albumEdit.getDescription());
+            album.setCategory(albumEdit.getCategory());
+            album.setTags(albumEdit.getTags());
+            albumRepository.save(album);
+            return ResponseEntity.ok().body("Album updated successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping(value = "/api/admin/album/audio", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> storeAudioFile(
+            @RequestParam("id") String id,
+            @RequestParam("name") String name,
+            @RequestParam("file") MultipartFile file) {
+
         try {
-            int status = objectStorageService.uploadObject("resources", "albums/"+fileName, file.getInputStream());
-            if(status == 200) {
-                return ResponseEntity.ok().body(fileName + " uploaded successfully");
-            }
+            storageService.putFile(Constants.albumsPath + "/" + id, name, file);
+            return ResponseEntity.status(201).body("Album song " + name + " uploaded successfully" );
         } catch (IOException e) {
             e.printStackTrace();
-            return ResponseEntity.internalServerError().body("Could not upload file " + fileName);
+            return ResponseEntity.internalServerError().body(e.getMessage());
         }
-        return ResponseEntity.ok().body(fileName + " uploaded successfully");
     }
+
+    @PostMapping(value = "/api/admin/album/artwork", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> storeAlbumArtwork(
+        @RequestParam("id") String id, 
+        @RequestParam("name") String name,
+        @RequestParam("file") MultipartFile file)
+        {
+        
+        try {
+            storageService.putFile(Constants.albumsPath + "/" + id, name, file);
+            return ResponseEntity.status(201).body("Album artwork uploaded successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    } 
+
+    @GetMapping("/api/admin/album/assets/audio/{dir}/{name}")
+    public ResponseEntity<FileSystemResource> getAudioFile(
+        @PathVariable("dir") String dir,
+        @PathVariable("name") String name
+        ) {
+        FileSystemResource fileSystemResource = storageService.readResourceFile(Constants.albumsPath + "/" + dir, name);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("audio/mpeg"))
+                .body(fileSystemResource);
+    }
+
+    @GetMapping("/api/admin/album/assets/artwork/{dir}/{name}")
+    public ResponseEntity<FileSystemResource> getAlbumArtwork(
+        @PathVariable("dir") String dir,
+        @PathVariable("name") String name
+    ) {
+        FileSystemResource fileSystemResource = storageService.readResourceFile(Constants.albumsPath + "/" + dir, name);
+        return ResponseEntity.ok()
+            .contentType(MediaType.IMAGE_JPEG)
+            .body(fileSystemResource);
+    }
+
+    @DeleteMapping("/api/admin/album/{id}")
+    public ResponseEntity<?> deleteAlbum(@PathVariable("id") String id) {
+        Optional<Album> album = albumRepository.findById(id);
+        if(album.isPresent()) {
+            albumRepository.delete(album.get());
+            try {
+                storageService.deleteFolder(Constants.albumsPath + "/" + id);
+                return ResponseEntity.ok().body("Asset deleted successfully");
+            } catch (IOException e) {
+                e.printStackTrace();
+                return ResponseEntity.internalServerError().body("Could not delete requested resource");
+            }
+        } else {
+            return ResponseEntity.ok().body("");
+        }
+    }
+    
 }

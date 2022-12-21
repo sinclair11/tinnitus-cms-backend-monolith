6a6b05a50dc0ddd88ea956e26a31c48d70b97896
Ștefan Țîncu <stefantnc11@gmail.com>
2022-12-21 22:42:24 +0200

    Implement API endpoints for sample resources
    Resolves: CU-8676zg6zy

--- src/main/java/com/tinnitussounds/cms/config/Constants.java
+++ src/main/java/com/tinnitussounds/cms/config/Constants.java
@@ -6,4 +6,5 @@ public class Constants {
     public final static String albumsPath = System.getProperty("user.dir") + "/assets/albums";
     public final static String categoriesPath = System.getProperty("user.dir") + "/assets/categories";
     public final static String presetsPath = System.getProperty("user.dir") + "/assets/presets";
+    public final static String samplesPath = System.getProperty("user.dir") + "/assets/samples";
 }

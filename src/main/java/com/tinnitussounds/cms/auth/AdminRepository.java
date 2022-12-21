4cb69eb508f3a80cd08dabf85a4430bbeb34564e
Ștefan Țîncu <stefantnc11@gmail.com>
2022-12-21 20:40:02 +0200

    Remove outdated method

--- src/main/java/com/tinnitussounds/cms/auth/AdminRepository.java
+++ src/main/java/com/tinnitussounds/cms/auth/AdminRepository.java
@@ -3,5 +3,4 @@ package com.tinnitussounds.cms.auth;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AdminRepository extends MongoRepository<Admin, String> {
-    public Admin findByUser(String user);
 }

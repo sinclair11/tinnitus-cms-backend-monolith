367746f9f89ccdc49bbe5d06f5e7486561c72ef6
Ștefan Țîncu <stefantnc11@gmail.com>
2022-12-22 10:37:48 +0200

    Add endpoint and query to search preset by name
    Resolves: CU-8676z5dev

--- src/main/java/com/tinnitussounds/cms/preset/PresetRepository.java
+++ src/main/java/com/tinnitussounds/cms/preset/PresetRepository.java
@@ -1,7 +1,11 @@
 package com.tinnitussounds.cms.preset;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface PresetRepository extends MongoRepository<Preset, String> {
-    
+    @Query("{ 'name' : ?0 }")
+    List<Preset> findByName(String name);
 }
